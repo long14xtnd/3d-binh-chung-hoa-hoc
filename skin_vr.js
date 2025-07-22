b1a820ab4963c9ee3940a2dbd4b90ca5e1638af4
long14xtnd <longdoan14xtnd@gmail.com>
2025-07-22 21:03:37 +0700

    config dong url backend list model

--- skin_vr.js
+++ skin_vr.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - VR - Skin
 // Pano2VR 7.0.9/20024
 // Filename: feather_vr.ggsk
-// Generated 2025-07-02T22:06:37
+// Generated 2025-07-22T21:02:13
 
 function pano2vrVrSkin(player,base) {
 	player.addVariable('node_cloner_vr_hasUp', 2, false, { ignoreInState: 0  });
